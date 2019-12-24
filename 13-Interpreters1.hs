0d9cf9dcc6bffd6da4e15ca3a7274238bf448b21
chuuck <gbolevics@gmail.com>
2019-12-24 15:13:33 +0000

    13th lecture on interpreters

---
+++ 13-Interpreters1.hs
@@ -0,0 +1,12 @@
+-- We say that a variable x is boun the term λ x -> e and the the score of this binding is
+-- term expression e
+
+-- A variable x in a term e that does not appear in the scope of any abstraction λ x -> is
+-- reffered to as free in e
+
+-- for example x, y are bound and z is free in λ x -> λ y -> x y z
+
+-- A λ calculus term thas no free variables is called closed
+-- Good explanation : https://lucasfcosta.com/2018/07/29/An-Introduction-to-Lambda-Calculus-Part-1.html
+
+-- Pretty much all you need to know about alpha and beta redux: https://lucasfcosta.com/2018/08/05/An-Introduction-to-Lambda-Calculus-Part-2.html
